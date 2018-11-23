038d07e33a5b46f377ad5f643309d8e9914704f8
arse <1033902477@qq.com>
2018-11-23 13:32:38 +0800

    修改代码的样子

--- common/DatabaseConnection/db.py
+++ common/DatabaseConnection/db.py
@@ -1,14 +1,8 @@
 import pymysql
 
+# 数据库连接的时候可以调用公共类方法进行连接
 class db():
     def DbConnection(self):
-        db = pymysql.connect(host='',
-                             user='',
-                             password='',
-                             port=3306,
-                             db='test',
-                             charset='utf8'
-                             )
+        db = pymysql.connect(host='', user='', password='', port=3306, db='test', charset='utf8')
         cursor = db.cursor()
-
         return cursor
