f2e9a43f9337cfa7bbb3672e63a89c216922cc2c
arse <44761213+1033902477@users.noreply.github.com>
2019-07-16 18:48:25 +0800

    Update Headers.py

--- Experiment/Common/Headers.py
+++ Experiment/Common/Headers.py
@@ -3,7 +3,6 @@ class Headers:
     def _headers(self):
 
         data = {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5kYXRhLmNhYXNkYXRhLmNvbS9hdXRoIiwiaWF0IjoxNTYyNTcxNTc1LCJleHAiOjE1NjUxNjM1NzUsIm5iZiI6MTU2MjU3MTU3NSwianRpIjoiR1RlaXpySmpZQVN2Sm9wNiIsInN1YiI6IjI0NzExIiwiTG9naW5Ub2tlbiI6IkNRWkR3eDhtQVBERTAifQ.GhqyRDyYvMFqfNTHxlKGZp7dR-W5mi9fE1aMkM0GXKk',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+            
         }
         return data
